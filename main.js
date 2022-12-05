544fa31f3feec14a524a1741e8e41860db121634
Asphy <arodriguezvera1997@gmail.com>
2022-12-05 10:31:32 -0300

    Se agregan funciones

--- main.js
+++ main.js
@@ -11,64 +11,95 @@ var noArroba = true;
 var tmpPassword = "";
 var change = '';
 const arroba = "@";
+var resetWhile = true;
 
-while (true) {
-   
-    if (contrasena == password && usuario == user && contrasena.length > 8) {
+function validarUsuario(contrasena, usuario) {
+
+    if (contrasena == password && usuario == user && contrasena.length > 8){
         alert(" La contraseña es segura ");
-        alert(" Bienvenido a nuestro sitio web " + usuario);
-        for (var i = 0; i < email.length; i++) {
-            if (arroba == email[i]) {
-                console.log("Se encotro un arroba");
-                email = prompt("ingrese su email");
-            } else {
-                if (noArroba) {
-                    alert("No se encontro un arroba");
-                    noArroba = false;
-                }
-            }
-        }
-        for (var i = 0; i < contrasena.length; i++) {
-            if (Number(contrasena[i])) {
-                alert(" Esta contraseña tiene un número ");
-                break;
-            }else{
-                
-                if (change.toLowerCase() == "si" && tmpPassword != contrasena) {
-                    alert('Su contraseña ha sido cambiada exitosamente');
-                    password = contrasena
-                    break
-                }else{
-                    change = prompt("Su contraseña debe tener caracteres alfanumericos. Desea cambiarla?");
-                    if (change.toLowerCase() == "si") {
-                        tmpPassword = contrasena;
-                        contrasena = prompt("Ingrese una nueva contraseña");
-                    }
-                    if (change.toLowerCase() == "no") {
-                        alert("Bienvenido nuevamente :)");
-                        break;
-                    }
-                }
-             
-
-                
-            }
-        }
-        console.log(contrasena, usuario, password, user);
-        break   
-        
-    } else if (contrasena != password && usuario == user) {
-        alert("Contraseña Incorrecta");
-        contrasena = prompt("Ingresa de forma correcta tu contraseña");
-    } else if (contrasena == password && usuario != user) {
-        alert("Usuario Incorrecto");
-        usuario = prompt("Ingresa de forma correcta tu usuario");
-    } else if (contrasena.length < 8) {
+        alert(`Bienvenido ${nombre}`);
+        validarEmail(email)
+        verificarContrasenaNumero(contrasena)
+       
+    } else if (validarSeguridad(contrasena)){
         alert("La contraseña es insegura");
         contrasena = prompt("Ingresa de forma correcta tu contraseña");
-    } else {
+    } else if (verificaUsuario(usuario, contrasena)) {
+        alert("Usuario Incorrecto");
+        usuario = prompt("Ingresa de forma correcta tu usuario");
+    } else if (verificarContrasena(contrasena)){
+        alert("Contraseña Incorrecta");
+        contrasena = prompt("Ingresa de forma correcta tu contraseña");
+    }else {
+        alert(`Lo sentimos ${nombre} pero tus datos no coinciden con los de ninguna cuenta registrada`);
         usuario = prompt("Ingresa de forma correcta tu usuario");
         contrasena = prompt("Ingresa de forma correcta tu contraseña");
-        alert("Usuario y/o Contraseña Incorrectos");
+        
     }
+}
+
+function validarEmail(email) {
+    for (var i = 0; i < email.length; i++) {
+        if (arroba == email[i]) {
+            console.log("Se encotro un arroba");
+            email = prompt("ingrese su email");
+        } else {
+            if (noArroba) {
+                alert("No se encontro un arroba");
+                noArroba = false;
+            }
+        }
+    }
+}
+
+function validarSeguridad(contrasena) {
+    if (contrasena.length < 8) {
+      return true
+    }
+    return false
+}
+
+function verificaUsuario(usuario, contrasena){
+    if (contrasena == password && usuario != user) {
+        return false
+    }
+}
+function verificarContrasena(contrasena){
+    if (contrasena != password && usuario == user) {
+        return false
+    }
+}
+
+function verificarContrasenaNumero(contrasena) {
+    for (var i = 0; i < contrasena.length; i++) {
+        if (Number(contrasena[i])) {
+            alert(" Esta contraseña tiene un número ");
+            break;
+        }else{
+            
+            if (change.toLowerCase() == "si" && tmpPassword != contrasena) {
+                alert('Su contraseña ha sido cambiada exitosamente');
+                password = contrasena
+                resetWhile = false;
+                alert("Bienvenido nuevamente :)");
+                break
+            }else{
+                change = prompt("Su contraseña debe tener caracteres alfanumericos. Desea cambiarla? (si/no)");
+                if (change.toLowerCase() == "si") {
+                    tmpPassword = contrasena;
+                    contrasena = prompt("Ingrese una nueva contraseña");
+                }
+                if (change.toLowerCase() == "no") {
+                    alert("Bienvenido nuevamente :)");
+                    resetWhile = false;
+                    break;
+                }
+            }    
+        }
+    }
+}
+
+
+while (resetWhile) {
+    validarUsuario(contrasena, usuario);
 }
